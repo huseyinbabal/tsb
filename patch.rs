e8e2ccef6264e85831c48ec92dbd97ebd8267a09
Hüseyin BABAL <huseyinbabal88@gmail.com>
2026-03-24 01:33:37 +0300

    chore: remove temporary patch files

--- patch.rs
+++
@@ -1,5 +0,0 @@
-const EMBEDDED_METADATA: &str = include_str!("../resources/initializr-metadata.json");
-const EMBEDDED_DEPENDENCIES: &str = include_str!("../resources/dependencies.json");
-
-const GITHUB_METADATA_URL: &str = "https://raw.githubusercontent.com/huseyinbabal/tsb/main/resources/initializr-metadata.json";
-const GITHUB_DEPENDENCIES_URL: &str = "https://raw.githubusercontent.com/huseyinbabal/tsb/main/resources/dependencies.json";
